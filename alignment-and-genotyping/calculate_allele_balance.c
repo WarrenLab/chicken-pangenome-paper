96edf757ea66e65421c9febad9dca555f5a57cf4
Edward S. Rice <edsrice@gmail.com>
2023-06-13 13:27:49 +0200

    use c for speedup

---
+++ alignment-and-genotyping/calculate_allele_balance.c
@@ -0,0 +1,85 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MIN_COVERAGE 10
+#define MAX_COVERAGE 100
+#define MAX_SEQNAME_LENGTH 200
+#define MAX_PILEUP_SIZE 8001
+
+float calcRefFreq(char *pileup) {
+  int i = 0;
+  int ref_bases = 0;
+  int nonref_A = 0;
+  int nonref_C = 0;
+  int nonref_G = 0;
+  int nonref_T = 0;
+  char c;
+
+  for (c = pileup[0]; c != '\0'; c = pileup[++i]) {
+    if (c == '.' || c == ',') {
+      ref_bases++;
+    }
+
+    else {
+      switch (toupper(c)) {
+      case 'A':
+        nonref_A++;
+        break;
+      case 'C':
+        nonref_C++;
+        break;
+      case 'G':
+        nonref_G++;
+        break;
+      case 'T':
+        nonref_T++;
+        break;
+      }
+    }
+  }
+
+  int total_bases = ref_bases + nonref_A + nonref_C + nonref_G + nonref_T;
+  int max_nonref = 0;
+
+  if (nonref_A > max_nonref) {
+    max_nonref = nonref_A;
+  }
+  if (nonref_C > max_nonref) {
+    max_nonref = nonref_C;
+  }
+  if (nonref_G > max_nonref) {
+    max_nonref = nonref_G;
+  }
+  if (nonref_T > max_nonref) {
+    max_nonref = nonref_T;
+  }
+
+  if ((max_nonref + ref_bases) * 1.0 / total_bases > 0.9 &&
+      (max_nonref * 1.0 / (ref_bases + max_nonref) > 0.25 ||
+       ref_bases * 1.0 / (ref_bases + max_nonref) > 0.25)) {
+    return max_nonref * 1.0 / (ref_bases + max_nonref);
+  }
+
+  return -1.0;
+}
+
+int main(int argc, char *argv[]) {
+  int position, coverage;
+  char base;
+  float refFreq;
+
+  char *seqName = malloc(MAX_SEQNAME_LENGTH * sizeof(char));
+  char *pileup = malloc(MAX_PILEUP_SIZE * sizeof(char));
+  char *quals = malloc(MAX_PILEUP_SIZE * sizeof(char));
+
+  while (scanf("%s\t%d\t%c\t%d\t%s\t%s", seqName, &position, &base, &coverage,
+               pileup, quals)) {
+    if (coverage >= MIN_COVERAGE && coverage <= MAX_COVERAGE) {
+      refFreq = calcRefFreq(pileup);
+      if (refFreq >= 0) {
+        printf("%f", refFreq);
+      }
+    }
+  }
+  return 0;
+}
